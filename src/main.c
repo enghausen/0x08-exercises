2d4a86dcd55d5e20630368c6e3fcda4af4a7787c
Morten Rode Enghausen <morten@enghausen.dk>
2025-04-17 13:24:44 +0200

    Løsning

--- src/main.c
+++ src/main.c
@@ -1,29 +1,69 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void init(int* buf, int buf_size);
+void init(int* buf, int count);
+void printBuffer(int* start, int size);
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <buffer_size> [values...]\n", argv[0]);
+        return 1;
+    }
 
-  int buffer[255]; 
-  // init buffer with a for loop and [] operator
+    // Buffer size from first argument
+    int buf_size = atoi(argv[1]);
+    if (buf_size <= 0 || buf_size > 255) {
+        printf("Invalid buffer size.\n");
+        return 1;
+    }
 
-  // declare three pointers: start, current, end
+    int buffer[255]; // max buffer
+    init(buffer, buf_size);
 
-  // while true
-    // take input from user
-    // update the value that current points to input
-    // if current is not at the end
-      // current = current + 1
-    // else
-      //current = start    
-    // print buffer 
+    // Circular buffer pointers
+    int* start = buffer;
+    int* end = buffer + buf_size;
+    int* current = buffer;
 
-  return 0;
+    // Fill initial values from argv[2] onward
+    for (int i = 2; i < argc; i++) {
+        int val = atoi(argv[i]);
+        *current = val;
+        printBuffer(buffer, buf_size);
+
+        current++;
+        if (current == end) {
+            current = start;
+        }
+    }
+
+    // Prompt user for new input
+    while (1) {
+        int val;
+        printf("Enter next value: ");
+        scanf("%d", &val);
+
+        *current = val;
+        printBuffer(buffer, buf_size);
+
+        current++;
+        if (current == end) {
+            current = start;
+        }
+    }
+
+    return 0;
 }
 
 void init(int* buf, int count) {
-  for(int i = 0; i < count; i++) {
-    buf[i] = 0;
-  }
+    for (int i = 0; i < count; i++) {
+        buf[i] = 0;
+    }
+}
+
+void printBuffer(int* buf, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", buf[i]);
+    }
+    printf("\n");
 }
